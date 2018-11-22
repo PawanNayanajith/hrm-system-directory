d47498f8f19aae0fcb554960644c9e116f87845c
fasla <faslafaseel@gmail.com>
2018-11-22 19:30:10 +0530

    CrossValidation Backend complete

--- hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/controller/DirectoryController.java
+++ hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/controller/DirectoryController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.sgic.hrm.commons.entity.User;
 import com.sgic.hrm.employee.service.DirectoryService;
 import com.sgic.hrm.employee.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class DirectoryController {
 
