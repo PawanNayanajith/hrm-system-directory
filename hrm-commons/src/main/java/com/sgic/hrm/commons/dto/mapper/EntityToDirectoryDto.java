672e3726c720d4e2106eb0afeebb8ab064e5edd4
PasinduSandakelum <tmpsthilakarathna2017@gmail.com>
2018-11-23 17:39:36 +0530

    Update directory DTO

--- hrm-commons/src/main/java/com/sgic/hrm/commons/dto/mapper/EntityToDirectoryDto.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/dto/mapper/EntityToDirectoryDto.java
@@ -1,21 +1,21 @@
 package com.sgic.hrm.commons.dto.mapper;
 
+import com.sgic.hrm.commons.dto.DirectoryDto;
 import com.sgic.hrm.commons.entity.User;
 
-import com.sgic.hrm.commons.dto.DirectoryDto;
-
 public class EntityToDirectoryDto {
- 
-	public static DirectoryDto entityToUserDtoMapping(User user) {
 
-		DirectoryDto userDto = new DirectoryDto();
-		userDto.setFullName(user.getFullName());
-		userDto.setEmail(user.getEmail());
-		userDto.setContact(user.getTelephoneNumber());
-		userDto.setPermenentAddress(user.getPermenentAddress());
-		userDto.setDesignation(user.getAppointmentId().getDesignationId().getDesignationName());
-		userDto.setAppointmentDate(user.getAppointmentId().getAppoinmentDate());
-		userDto.setServicePeriod(user.getServicePeriod());
-		return userDto;
-	}
+  public static DirectoryDto entityToUserDtoMapping(User user) {
+
+    DirectoryDto userDto = new DirectoryDto();
+    userDto.setUserId(user.getId());
+    userDto.setFullName(user.getFullName());
+    userDto.setEmail(user.getEmail());
+    userDto.setContact(user.getTelephoneNumber());
+    userDto.setPermenentAddress(user.getPermenentAddress());
+    userDto.setDesignation(user.getAppointmentId().getDesignationId().getDesignationName());
+    userDto.setAppointmentDate(user.getAppointmentId().getAppoinmentDate());
+    userDto.setServicePeriod(user.getServicePeriod());
+    return userDto;
+  }
 }
